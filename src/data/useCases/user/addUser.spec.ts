31dad9c24530a70df396541ab496db298395ee93
Edlâneo Júlio <edlanioj@gmail.com>
2021-01-04 16:31:32 +0100

    feat: ensure AddUserUseCase call GetUserByEmailRepository on creation

---
+++ src/data/useCases/user/addUser.spec.ts
@@ -0,0 +1,54 @@
+import faker from 'faker';
+
+import { GetUserByEmailRepository } from '@/data/protocols/db/user/getUserByEmail';
+import { GetUserByEmailRepositorySpy } from '@/data/tests/db/user/getUserEmailRepositorySpy';
+import { AddUser } from '@/domain/useCases/user/addUser';
+
+interface Hasher {
+  hash: (plaintext: string) => Promise<void>;
+}
+
+class HasherSpy implements Hasher {
+  plaintext = '';
+
+  async hash(plaintext: string): Promise<void> {
+    this.plaintext = plaintext;
+  }
+}
+
+class AddUserUseCase {
+  constructor(
+    private readonly getUserByEmailRepository: GetUserByEmailRepository
+  ) {}
+
+  async add(params: AddUser.Params): Promise<void> {
+    await this.getUserByEmailRepository.getByEmail(params.email);
+  }
+}
+type SutType = {
+  sut: AddUserUseCase;
+  getUserByEmailRepositorySpy: GetUserByEmailRepositorySpy;
+};
+const makeSut = (): SutType => {
+  const getUserByEmailRepositorySpy = new GetUserByEmailRepositorySpy();
+  const sut = new AddUserUseCase(getUserByEmailRepositorySpy);
+
+  return { getUserByEmailRepositorySpy, sut };
+};
+
+const mockAddUserParams = (): AddUser.Params => ({
+  email: faker.internet.email(),
+  name: faker.name.findName(),
+  password: faker.random.alphaNumeric(8),
+  username: faker.internet.userName(),
+});
+describe('AddUser use case', () => {
+  it('Should call GetUserByEmailRepository with correct email', async () => {
+    const { sut, getUserByEmailRepositorySpy } = makeSut();
+    const addUserParams = mockAddUserParams();
+
+    await sut.add(addUserParams);
+
+    expect(getUserByEmailRepositorySpy.email).toBe(addUserParams.email);
+  });
+});
