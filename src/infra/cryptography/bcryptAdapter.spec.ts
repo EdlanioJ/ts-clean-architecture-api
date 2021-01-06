a3313f760d3f73a283508666532d529e16cc338f
Edlâneo Júlio <edlanioj@gmail.com>
2021-01-06 15:41:44 +0100

    feat: ensure bcryptAdater.hash return a valid hash on hash success

--- src/infra/cryptography/bcryptAdapter.spec.ts
+++ src/infra/cryptography/bcryptAdapter.spec.ts
@@ -1,10 +1,19 @@
 import bcrypt from 'bcryptjs';
 
-class BcryptAdapter {
+import { Hasher } from '@/data/protocols/cryptography/hasher';
+
+jest.mock('bcryptjs', () => ({
+  async hash(): Promise<string> {
+    return 'hash';
+  },
+}));
+class BcryptAdapter implements Hasher {
   constructor(private readonly salt: number) {}
 
-  async hash(plaintext: string): Promise<void> {
-    await bcrypt.hash(plaintext, this.salt);
+  async hash(plaintext: string): Promise<string> {
+    const digest = await bcrypt.hash(plaintext, this.salt);
+
+    return digest;
   }
 }
 const salt = 12;
@@ -20,6 +29,13 @@ describe('Bcrypt Adapter', () => {
       await sut.hash('any_value');
 
       expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
+    });
+
+    it('Should return a valid hash on hash success', async () => {
+      const sut = makeSut();
+      const hash = await sut.hash('any_value');
+
+      expect(hash).toBe('hash');
     });
   });
 });
