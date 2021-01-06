4b571e3d344efd3572281ad270d604ce17516a14
Edlâneo Júlio <edlanioj@gmail.com>
2021-01-06 17:25:41 +0100

    feat: ensure jwtAdapter.encrypt throw if sign throws

--- src/infra/cryptography/jwtAdapter.spec.ts
+++ src/infra/cryptography/jwtAdapter.spec.ts
@@ -35,5 +35,14 @@ describe('jwt Adaper', () => {
       const accessToken = await sut.encrypt('any_id');
       expect(accessToken).toBe('any_token');
     });
+
+    it('Should throw if sign throws', async () => {
+      const sut = makeSut();
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+        throw new Error();
+      });
+      const promise = sut.encrypt('any_id');
+      await expect(promise).rejects.toThrow();
+    });
   });
 });
