d1a5b07602f6a6d98cdc738e7d29592c1ad0986c
Edlâneo Júlio <edlanioj@gmail.com>
2021-01-06 16:44:34 +0100

    feat: ensure bcryptAdaper.compare throw if compare throws

--- src/infra/cryptography/bcryptAdapter.spec.ts
+++ src/infra/cryptography/bcryptAdapter.spec.ts
@@ -82,5 +82,14 @@ describe('Bcrypt Adapter', () => {
 
       expect(isValid).toBe(false);
     });
+
+    it('Should throw if compare throws', async () => {
+      const sut = makeSut();
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => {
+        throw new Error();
+      });
+      const promise = sut.compare('any_value', 'any_hash');
+      await expect(promise).rejects.toThrow();
+    });
   });
 });
