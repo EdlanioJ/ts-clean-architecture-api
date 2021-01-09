cb7ae1ed81ba780c6f89c80cd425162f98a775d2
Edlâneo Júlio <edlanioj@gmail.com>
2021-01-09 11:12:37 +0100

    feat: ensure userPrismaRepository.add call create with correct values

---
+++ src/infra/db/prisma/user/userPrismaRepository.spec.ts
@@ -0,0 +1,67 @@
+import faker from 'faker';
+
+import { AddUserRepositoryParams } from '@/data/protocols/db/user/addUserRepository';
+import { PrismaClient, Prisma } from '@prisma/client';
+
+// Must *CHANGE IT*
+class PrismaUserSpy {
+  createValue: object = {} as Prisma.userCreateArgs;
+
+  async create(value: Prisma.userCreateArgs): Promise<void> {
+    this.createValue = value;
+  }
+}
+
+const prismaUserSpy = new PrismaUserSpy();
+
+jest.mock('@prisma/client', () => ({
+  PrismaClient: class {
+    user = {
+      create: (value: Prisma.userCreateArgs) => prismaUserSpy.create(value),
+    };
+  },
+}));
+
+class UserPrismaRepository {
+  constructor(private readonly prisma = new PrismaClient()) {}
+
+  async add(params: AddUserRepositoryParams): Promise<void> {
+    const { email, id, name, password, username } = params;
+    await this.prisma.user.create({
+      data: {
+        email,
+        id,
+        name,
+        password,
+        username,
+      },
+    });
+  }
+}
+
+const makeSut = (): UserPrismaRepository => {
+  return new UserPrismaRepository();
+};
+const mockAddUserRepositoryParams = (): AddUserRepositoryParams => ({
+  email: faker.internet.email(),
+  id: faker.random.uuid(),
+  name: faker.name.findName(),
+  password: faker.random.uuid(),
+  username: faker.internet.userName(),
+});
+
+describe('UserPrismaRepository', () => {
+  describe('add()', () => {
+    it('Should call create with correct values', async () => {
+      const sut = makeSut();
+      const addUserRepositoryParams = mockAddUserRepositoryParams();
+      const user = await sut.add(addUserRepositoryParams);
+
+      expect(prismaUserSpy.createValue).toEqual(
+        expect.objectContaining({
+          data: addUserRepositoryParams,
+        })
+      );
+    });
+  });
+});
