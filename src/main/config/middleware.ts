8f340c35689ee381c6c6b636be772961859e9edc
Edlâneo Júlio <edlanioj@gmail.com>
2021-01-12 20:01:33 +0100

    feat: add content type middleware

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,9 +1,11 @@
 import { Express } from 'express';
 
 import { bodyParser } from '../middlewares/bodyParser';
+import { contentType } from '../middlewares/contentType';
 import { cors } from '../middlewares/cors';
 
 export const setupMiddlewares = (app: Express) => {
   app.use(bodyParser);
   app.use(cors);
+  app.use(contentType);
 };
