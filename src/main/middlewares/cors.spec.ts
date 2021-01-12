feca1eaf47b67994921d1f0a6b866dd441713d71
Edlâneo Júlio <edlanioj@gmail.com>
2021-01-12 19:44:59 +0100

    refactor: update test closure

--- src/main/middlewares/cors.spec.ts
+++ src/main/middlewares/cors.spec.ts
@@ -3,7 +3,7 @@ import request from 'supertest';
 import app from '../config/app';
 
 describe('Cors Middleware', () => {
-  test('Should enable CORS', async () => {
+  it('Should enable CORS', async () => {
     app.get('/test_cors', (req, res) => {
       res.send();
     });
