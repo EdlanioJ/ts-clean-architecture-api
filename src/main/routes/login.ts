bbf34bb4773b39313154f320dcd023a85e7a72ae
Edlâneo Júlio <edlanioj@gmail.com>
2021-01-13 16:43:17 +0100

    feat: add register route in main layer

--- src/main/routes/login.ts
+++ src/main/routes/login.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 
 import { adaptRoute } from '../adapers/route';
+import { makeAddUserController } from '../factories/user/addUser';
 import { makeAuthenticationController } from '../factories/user/authentication';
 
 export default (router: Router): void => {
   router.post('/auth', adaptRoute(makeAuthenticationController()));
+  router.post('/register', adaptRoute(makeAddUserController()));
 };
