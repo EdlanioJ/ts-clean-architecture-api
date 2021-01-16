269d51a8416f77e3bac9d0aa9582b814fc2d54b7
Edlâneo Júlio <edlanioj@gmail.com>
2021-01-16 21:16:37 +0100

    feat: ensure ForgotPassword Service throw if generateToken.generate throw

--- src/data/services/user/forgotPassword.spec.ts
+++ src/data/services/user/forgotPassword.spec.ts
@@ -1,28 +1,54 @@
+import faker from 'faker';
+
+import { GenerateToken } from '@/data/protocols/cryptography/generateToken';
 import { UserRepository } from '@/data/protocols/db/user/userRepository';
 import { mockAddUserParams } from '@/data/tests/db/user/mockAddUserParams';
 import { UserRepositorySpy } from '@/data/tests/db/user/userRepositorySpy';
 import { UnauthorizedError } from '@/domain/errors/user/unauthorized';
 
 class ForgotPasswordService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly generateToken: GenerateToken
+  ) {}
 
   async add(email: string): Promise<void> {
     const user = await this.userRepository.getByEmail(email);
 
     if (!user) throw new UnauthorizedError('email');
+
+    await this.generateToken.generate();
+  }
+}
+
+class GenerateTokenSpy implements GenerateToken {
+  token = faker.random.uuid();
+
+  async generate(): Promise<string> {
+    return this.token;
+  }
+
+  simulateThrowError() {
+    jest
+      .spyOn(GenerateTokenSpy.prototype, 'generate')
+      .mockImplementationOnce(() => {
+        throw new Error();
+      });
   }
 }
 
 type SutType = {
   sut: ForgotPasswordService;
   userRepositorySpy: UserRepositorySpy;
+  generateTokenSpy: GenerateTokenSpy;
 };
 
 const makeSut = (): SutType => {
   const userRepositorySpy = new UserRepositorySpy();
-  const sut = new ForgotPasswordService(userRepositorySpy);
+  const generateTokenSpy = new GenerateTokenSpy();
+  const sut = new ForgotPasswordService(userRepositorySpy, generateTokenSpy);
 
-  return { sut, userRepositorySpy };
+  return { sut, userRepositorySpy, generateTokenSpy };
 };
 describe('Forgot Password Service', () => {
   it('Should call UserRepository getUserByEmail with correct email', async () => {
@@ -51,5 +77,15 @@ describe('Forgot Password Service', () => {
     const promise = sut.add(mockAddUserParams().email);
 
     await expect(promise).rejects.toThrow(new UnauthorizedError('email'));
+  });
+
+  it('Should throw if generateToken.generate throw', async () => {
+    const { generateTokenSpy, sut, userRepositorySpy } = makeSut();
+
+    generateTokenSpy.simulateThrowError();
+
+    const promise = sut.add(mockAddUserParams().email);
+
+    await expect(promise).rejects.toThrowError();
   });
 });
