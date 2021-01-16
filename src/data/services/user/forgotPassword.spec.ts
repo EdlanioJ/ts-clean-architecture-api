9dc39851b25d25eeaf5f3cd239dffc671c548a6a
Edlâneo Júlio <edlanioj@gmail.com>
2021-01-16 19:33:51 +0100

    feat: ensure ForgotPassword Service call UserRepository getUserByEmail with correct email

---
+++ src/data/services/user/forgotPassword.spec.ts
@@ -0,0 +1,33 @@
+import { UserRepository } from '@/data/protocols/db/user/userRepository';
+import { mockAddUserParams } from '@/data/tests/db/user/mockAddUserParams';
+import { UserRepositorySpy } from '@/data/tests/db/user/userRepositorySpy';
+
+class ForgotPasswordService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async add(email: string): Promise<void> {
+    await this.userRepository.getByEmail(email);
+  }
+}
+
+type SutType = {
+  sut: ForgotPasswordService;
+  userRepositorySpy: UserRepositorySpy;
+};
+
+const makeSut = (): SutType => {
+  const userRepositorySpy = new UserRepositorySpy();
+  const sut = new ForgotPasswordService(userRepositorySpy);
+
+  return { sut, userRepositorySpy };
+};
+describe('Forgot Password Service', () => {
+  it('Should call UserRepository getUserByEmail with correct email', async () => {
+    const { sut, userRepositorySpy } = makeSut();
+    const addUserParams = mockAddUserParams();
+
+    await sut.add(addUserParams.email);
+
+    expect(userRepositorySpy.email).toBe(addUserParams.email);
+  });
+});
