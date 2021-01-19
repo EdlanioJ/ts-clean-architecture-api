ad4db29f61b250a8edc7596c59a047db9d7f5f47
Edlâneo Júlio <edlanioj@gmail.com>
2021-01-19 16:03:30 +0100

    feat: add forgot-password route

--- src/main/routes/login.ts
+++ src/main/routes/login.ts
@@ -3,8 +3,10 @@ import { Router } from 'express';
 import { adaptRoute } from '../adapers/route';
 import { makeAddUserController } from '../factories/user/addUser';
 import { makeAuthenticationController } from '../factories/user/authentication';
+import { makeForgotPasswordController } from '../factories/user/forgotPassword';
 
 export default (router: Router): void => {
   router.post('/auth', adaptRoute(makeAuthenticationController()));
   router.post('/register', adaptRoute(makeAddUserController()));
+  router.post('/forgot-password', adaptRoute(makeForgotPasswordController()));
 };
