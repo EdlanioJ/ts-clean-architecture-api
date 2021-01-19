ca004c6bc6724af8c305e2b8d91f95b16a895454
Edlâneo Júlio <edlanioj@gmail.com>
2021-01-19 14:50:44 +0100

    feat: add kafka config to env file

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,9 @@
+import { KafkaConfig, ProducerConfig } from 'kafkajs';
+
 export const env = {
   port: process.env.PORT || 3333,
   salt: Number(process.env.BCRYPT_SALT),
   jwtSecret: process.env.APP_SECRET,
+  kafka: {} as KafkaConfig,
+  kafkaProducer: {} as ProducerConfig,
 };
