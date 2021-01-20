6731069dbf1845811efe5fd88a8bea2656c5fdf1
Edlâneo Júlio <edlanioj@gmail.com>
2021-01-20 20:13:58 +0100

    feat: add kafka and kafka producer config

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,6 +4,12 @@ export const env = {
   port: process.env.PORT || 3333,
   salt: Number(process.env.BCRYPT_SALT),
   jwtSecret: process.env.APP_SECRET,
-  kafka: {} as KafkaConfig,
-  kafkaProducer: {} as ProducerConfig,
+  kafka: {
+    clientId: process.env.KAFKA_CLIENT_ID,
+    brokers: ['localhost:9092'],
+    retry: { retries: 5, initialRetryTime: 300 },
+  } as KafkaConfig,
+  kafkaProducer: {
+    allowAutoTopicCreation: true,
+  } as ProducerConfig,
 };
