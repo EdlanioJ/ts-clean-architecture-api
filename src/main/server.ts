6bd819ae23d2fd03de96f003b08acd26991ef745
Edlâneo Júlio <edlanioj@gmail.com>
2021-01-20 20:08:41 +0100

    fix: repair env import

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,5 @@
-import { env } from '@/main//config/env';
 import app from '@/main/config/app';
+import { env } from '@/main/config/env';
 
 app.listen(env.port, () =>
   console.log(`Server running at http://localhost:${env.port}`)
